4a221d9ef9ed290ea5592c49cd227ef976e68ab0
Jorge A. Borges <contact@jorgeborges.me>
2020-09-26 15:39:01 +1000

    feat: display accounts and balances

--- playground/web3test/index.js
+++ playground/web3test/index.js
@@ -1,5 +1,20 @@
 const Web3 = require('web3');
 
-const web3 = new Web3('http://127.0.0.1:7545');
 
-web3.eth.getAccounts().then(accounts => console.log(accounts));
+async function run() {
+  const web3 = new Web3('http://127.0.0.1:7545');
+
+  /** @type {Array} */
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('The accounts: ', accounts);
+
+  for (const account of accounts) {
+    console.log(`For account ${account}`);
+    const balance = await web3.eth.getBalance(account);
+    console.log(`ETH Balance: ${web3.utils.fromWei(balance)}`)
+    console.log('---')
+  }
+}
+
+run();
